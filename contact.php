bf84b4c320adbd54f7f2323f517bbf6300ebeddf
aquaventures <152424775+aquaventures@users.noreply.github.com>
2023-11-29 17:58:37 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,9 @@
+<div class="contact-info">
+      <h3>CONTACT US!</h3>
+      <p>AQUA ADVENTURES</p>
+      <p>WATERSPORT RENTAL</p>
+      <p><a href="https://maps.google.com/maps?q=5575%20NORTH%20ATLANTIC%20AVE,%20COCOA%20BEACH%203291" target="_blank"><i class="fas fa-map-marker-alt"></i> 5575 NORTH ATLANTIC AVE, COCOA BEACH 32931</a></p>
+      <p><i class="fas fa-phone"></i> <a href="tel:350-456-2628">831-456-2628</a></p>
+      <p><i class="fas fa-envelope"></i> <a href="mailto:support@aquarentals.com">support@aquarentals.com</a></p>
+    </div>
+  </div>
